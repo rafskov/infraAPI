1ca58074c2148c00192917b6705bbbdfe5eefe12
Rafael Skovron <rafskov@github.com>
2023-03-18 06:15:27 +0000

    still need to fix the dataload from csv

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 from flask import Flask, jsonify
-
-#create a sqlite database to store the data
 import sqlite3
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
 
-#create a table with columns for county, state, year
-c.execute('''CREATE TABLE data (county text, state text, year integer)''')
+#check if the database exists
+import os
+if not os.path.exists('data.db'):
+    #if not, create it
 
-#load data from poweroutage.csv into the database
-import csv
-with open('poweroutage.csv', 'r') as f:
-    reader = csv.reader(f)
-    next(reader) #skip header
-    for row in reader:
-        c.execute("INSERT INTO data VALUES (?, ?, ?)", row)
+    #create a sqlite database to store the data
+    import sqlite3
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
 
-#create a flask API to query the database
+    #create a table with columns for county, state, year
+    c.execute('''CREATE TABLE data (county text, state text, year integer)''')
+
+    #load data from poweroutage.csv into the database
+    import csv
+    with open('poweroutage.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader) #skip header
+        for row in reader:
+            c.execute("INSERT INTO data VALUES (?, ?, ?)", row)
+
+#create a flask API to query the data table in the sqlite database
 app = Flask(__name__)
-@app.route('/api/<county>/<state>/<year>')
-def api(county, state, year):
-    c.execute("SELECT * FROM data WHERE county=? AND state=? AND year=?", (county, state, year))
-    return jsonify(c.fetchall())
-
+@app.route('/api/<county>/<state>/')
+def api(county,state): #create docstring
+    """Return the year of the last power outage in a county in a state"""
+    #create conn for the database
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM data WHERE county=? AND state=?",(county, state))
+    row = c.fetchone()
+    if row:
+        return jsonify({'county': row[0], 'state': row[1], 'year': row[2]})
+    else:
+        return jsonify({'error': 'not found'})
+    
